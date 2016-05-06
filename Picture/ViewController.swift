b145152123deed8a4cefcc3dd7b55b16076f9270
朱莹浩 <zhuyinghao@zhuyinghaodemacbook-pro.local>
2016-05-06 18:50:46 +0800

    Initial Commit

---
+++ Picture/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Picture
+//
+//  Created by 朱莹浩 on 5/6/16.
+//  Copyright © 2016 朱莹浩. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
